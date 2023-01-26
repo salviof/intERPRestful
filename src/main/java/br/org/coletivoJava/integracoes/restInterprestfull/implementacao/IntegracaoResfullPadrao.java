dc93658724d568432717fa37b02dc63ebbad1c39
Salvio Furbino <salviof@gmail.com>
2023-01-26 01:23:59 -0300

    Atualizacao automática

--- src/main/java/br/org/coletivoJava/integracoes/restInterprestfull/implementacao/IntegracaoResfullPadrao.java
+++ src/main/java/br/org/coletivoJava/integracoes/restInterprestfull/implementacao/IntegracaoResfullPadrao.java
@@ -64,7 +64,7 @@ public class IntegracaoResfullPadrao extends
     }
 
     @Override
-    protected RespostaWebServiceSimples buildResposta(RespostaWebServiceSimples pRespostaWSSemTratamento) {
+    protected RespostaWebServiceSimples gerarRespostaTratamentoFino(RespostaWebServiceSimples pRespostaWSSemTratamento) {
         if (!UtilSBCoreStringValidador.isNuloOuEmbranco(pRespostaWSSemTratamento.getRetorno())) {
             pRespostaWSSemTratamento.getMensagens();
             try {
